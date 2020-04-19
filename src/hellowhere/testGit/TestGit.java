3e767fbcf5b0d9283a987f84f6f7db8eca6da24b
zengxianggao123 <854885869@qq.com>
2020-04-19 17:42:46 +0800

    tijiao

--- src/hellowhere/testGit/TestGit.java
+++ src/hellowhere/testGit/TestGit.java
@@ -4,6 +4,10 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("wode 脢脌陆莽脦脪脳枚脰梅");
 		System.out.println("dsfsdfdsf");
+		TestGit.show();
+	}
+	public static void show() {
+		System.out.println("helloWord");
 	}
 
 }
